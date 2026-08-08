e4d0e0e67b999580880e6af17ec918227ca0d3a6
kongoza22 <158481304+kongoza22@users.noreply.github.com>
2026-08-09 00:04:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
-// Enregistrement des routes avec le préfixe /api
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/rooms', require('./routes/roomRoutes'));
-app.use('/api/staff', require('./routes/staffRoutes'));
+// Enregistrement des routes correspondant aux appels du frontend
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/rooms', require('./routes/roomRoutes'));
+app.use('/staff', require('./routes/staffRoutes'));
 
 app.listen(PORT, () => {
   console.log(`Serveur Enterprise PMS démarré sur le port ${PORT}`);
